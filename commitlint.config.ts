7ff32aaa698f07b32a9c94a281bf4928add6f490
HunterDev619 <VuHaiDang619@outlook.com>
2026-02-23 03:46:55 +0800

    Update Refreshing

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,5 +4,5 @@ const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   ignores: [message => message.startsWith('chore: bump')], // Ignore dependabot commits
 };
-
+//  this
 export default Configuration;
